e5a41c7884129d32cbc99a5f2fa0e0429fbe998a
Marvan Taghvaie & Nestor Alvarez <pair+marvant.xdkernelx@devbootcamp.com>
2016-10-24 15:36:56 -0700

    Write migration file for User class.

--- db/migrate/20161024223340_create_users.rb
+++ db/migrate/20161024223340_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :username, { limit: 128 }
+      t.string :email, { limit: 128 }
+      t.string :hashed_password, { limit: 64 }
+
+      t.timestamps(null: false)
     end
   end
 end
